2521e26360d38d4b2c2cc7931aa441b61a9506d0
kikichau <kiki.1155@hotmail.com>
2019-04-27 18:46:37 +0800

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: 'index',                component: ComponentsComponent },
+    { path: 'nucleoicons',          component: NucleoiconsComponent },
+    { path: 'landing',     component: LandingComponent },
+    { path: 'login',                component: LoginComponent },
+    { path: 'register',             component: RegisterComponent },
+    { path: 'profile',     component: ProfileComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+    ],
+})
+export class AppRoutingModule { }
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NouisliderModule } from 'ng2-nouislider';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+import { AgmCoreModule } from '@agm/core';
+
+import { LoginComponent } from '../user/login/login.component';
+import { RegisterComponent } from '../user/register/register.component';
+import { ProfileComponent } from '../user/profile/profile.component';
+import { UserComponent } from './user.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        NgbModule,
+        NouisliderModule,
+        JwBootstrapSwitchNg2Module,
+        AgmCoreModule.forRoot({
+            apiKey: 'YOUR_KEY_HERE'
+        })
+    ],
+    declarations: [
+        LoginComponent,
+        RegisterComponent,
+        UserComponent,
+        ProfileComponent
+    ]
+})
+export class UserModule { }
